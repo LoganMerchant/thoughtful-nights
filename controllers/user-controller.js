4ea16e1a27f6cb27c97bb332fe09915ba0169d99
Logan Merchant <merchantclogan@gmail.com>
2020-12-05 03:00:00 -0700

    add all routes for /api/users

---
+++ controllers/user-controller.js
@@ -0,0 +1,75 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+  getAllUsers(req, res) {
+    User.find({})
+      .select("-__v")
+      .populate({
+        path: "thoughts",
+        path: "users",
+        select: "-__v",
+      })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  getOneUser({ params }, res) {
+    User.find({ _id: params.id })
+      .select("-__v")
+      .populate({
+        path: "thoughts",
+        path: 'users',
+        select: "-__v",
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id." });
+          return;
+        }
+
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found!" });
+          return;
+        }
+
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+module.exports = userController;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { getAllUsers, getOneUser, createUser, updateUser, deleteUser } = require('../../controllers/user-controller');
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route('/:id')
+    .get(getOneUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
